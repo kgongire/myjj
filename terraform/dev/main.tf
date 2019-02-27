4125995faac60ebc827c1ab6ab6fe8153fe6ed94
kgongire <47124838+kgongire@users.noreply.github.com>
2019-02-26 16:12:27 -0800

    Update main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region  = "us-west-1"
-  aws_access_key_id = AKIAJ6PWUOUEWTK7XWCQ
-  aws_secret_access_key = 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
+  profile = "mycdd"
   version = "1.23.0"
 }
 
