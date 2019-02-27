c5a9046e648ea1e26f7fd40d76a1f4f86bf82ca4
kgongire <47124838+kgongire@users.noreply.github.com>
2019-02-26 16:12:53 -0800

    Update tfbackend.tf

--- terraform/dev/tfbackend.tf
+++ terraform/dev/tfbackend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket  = "bucket2174"
     key     = "tf/dev.json"
     region  = "us-west-1"
-    aws_access_key_id = AKIAJ6PWUOUEWTK7XWCQ
-    aws_secret_access_key = 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
+    profile = "mycdd"
   }
 }
